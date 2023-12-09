c7918c0a33a8b1a14c83235a80820d783567d9fb
Gabriel Flores <gabriel.openix@infocorp.microsoftonline.com>
2023-12-09 08:46:33 -0300

    Redirección a business

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -5,12 +5,12 @@ import { Overlap4Component }from '../overlap4/overlap4.component';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
 import { OurServicesComponent } from '../our-services/our-services.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [OverlapComponent,NavBarComponent,OurServicesComponent,Overlap3Component,Overlap4Component,NavBarComponent,CommonModule, RouterOutlet],
+  imports: [OverlapComponent,NavBarComponent,OurServicesComponent,Overlap3Component,Overlap4Component,NavBarComponent,CommonModule, RouterOutlet, RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
   host: {ngSkipHydration: 'true'},
