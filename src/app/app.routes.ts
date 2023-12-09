2bc98c4a2dec56aa25ecd1909af15e602f00f9ce
ignacio <ignacio@openix.com.ar>
2023-12-09 02:22:33 -0300

    #Business Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { OverlapComponent } from './components/overlap/overlap.component';
 import { Overlap2Component } from './components/overlap2/overlap2.component';
 import { Overlap3Component } from './components/overlap3/overlap3.component';
 import { Overlap4Component } from './components/overlap4/overlap4.component';
+import { BusinessComponent } from './components/business/business.component';
 
 
 
@@ -13,4 +14,5 @@ export const routes: Routes = [{path: 'app-main', component: MainComponent},
 {path: 'app-overlap', component: OverlapComponent },
 {path: 'app-overlap2', component: Overlap2Component},
 {path: 'app-overlap3', component: Overlap3Component},
-{path: 'app-overlap4', component: Overlap4Component}];
+{path: 'app-overlap4', component: Overlap4Component},
+{path: 'app-business', component: BusinessComponent}];
